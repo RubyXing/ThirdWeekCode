45ca340012addc11491414b0ba2d19c837e044e1
RubyXing <593680319@qq.com>
2020-01-04 17:03:15 +0800

    Commit from class PC

--- Day6Code/src/Launcer6.java
+++ Day6Code/src/Launcer6.java
@@ -1,25 +1,28 @@
-import expcatch.FakerBank;
-import expcatch.InsufficientFoundException;
-import expcatch.NagativateFoundException;
-
 public class Launcer6 {
     public static void main(String[] args) {
 //        TwoExpCatch two=new TwoExpCatch();
 //        two.catchexp();
 
-        FakerBank fb = new FakerBank(100);
+//        FakerBank fb = new FakerBank(100);
+//        try {
+//            fb.withDrawal(-26);
+//        } catch (InsufficientFoundException | NagativateFoundException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            fb.withDrawal(130);
+//        } catch (NagativateFoundException | InsufficientFoundException e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            fb.withDrawal(-26);
-        } catch (InsufficientFoundException | NagativateFoundException e) {
-            e.printStackTrace();
-        }
+//        File fl=new File("testfile.txt");
+//        File fl2=new File("d:\\targetfile");
+//        System.out.println(fl2.canRead());
+//        System.out.println(fl2.canExecute());
+//        System.out.println(fl2.getAbsoluteFile());
+//        System.out.println(fl2.getAbsolutePath());
+//        System.out.println(fl2.getPath());
+//        System.out.println(fl2.exists());
 
-
-        try {
-            fb.withDrawal(130);
-        } catch (NagativateFoundException | InsufficientFoundException e) {
-            e.printStackTrace();
-        }
     }
 }
